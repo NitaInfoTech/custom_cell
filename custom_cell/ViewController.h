09c9b6b6c0a5ac2d389a3b4aa25bcdd58a28a0a4
MACOS <macos@macoss-imac-26.local>
2016-09-02 10:35:42 +0530

    Initial Commit

---
+++ custom_cell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  custom_cell
+//
+//  Created by MACOS on 9/2/16.
+//  Copyright © 2016 MACOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
